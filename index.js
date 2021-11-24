a5d379719dcbc17327d63529b5bea2390ae775ab
Adriano Campos <adrianohcampos@gmail.com>
2021-11-24 17:48:38 -0300

    update

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const time = new Date().getTime();
 app.get("/pdf", async (req, res) => {
 
     const url = req.query.target;
+    const key = req.query.target
 
-
-
-    // Create an instance of the chrome browser
-    const browser = await puppeteer.launch({
-        headless: true
-    });
+    if (key != )
+        // Create an instance of the chrome browser
+        const browser = await puppeteer.launch({
+            headless: true
+        });
 
     // Create a new page
     const webPage = await browser.newPage();
@@ -28,14 +28,15 @@ app.get("/pdf", async (req, res) => {
         waitUntil: "networkidle0"
     });
 
+    // Set filename
     let pageTitle = await webPage.title();
     pageTitle = pageTitle.replace(/[^a-zA-Z0-9]/g, '_');
     pageTitle = pageTitle.toLowerCase();
+    const filename = pageTitle + '_' + time + '.pdf';
 
-    const newname = pageTitle + '_' + time + '.pdf';
-
+    // Set pdf file name
     const pdfConfig = {
-        path: newname, // Saves pdf to disk. 
+        path: filename, // Saves pdf to disk. 
         format: 'A4',
         printBackground: true,
         margin: { // Word's default A4 margins
@@ -55,13 +56,10 @@ app.get("/pdf", async (req, res) => {
     // const urlaws = await s3Client.uploadFile(newname, newname);
 
     const json = JSON.stringify({
-        url: url,
-        file: newname,
+        target: url,
+        file: filename,
         // aws: urlaws
     });
-
-    // remove temp file
-    //fs.unlinkSync(newname);
 
     res.contentType("application/json");
     res.send(json);
