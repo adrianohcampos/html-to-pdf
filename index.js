f2389693a97e249fbb997de1fa90eb1018672ad3
Adriano Campos <adrianohcampos@gmail.com>
2021-11-24 18:00:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,57 +9,68 @@ const time = new Date().getTime();
 app.get("/pdf", async (req, res) => {
 
     const url = req.query.target;
-    const key = req.query.target
+    const key = req.query.key
+    let json = '';
 
-    if (key != )
+    if (key == process.env.KEY) {
+
         // Create an instance of the chrome browser
         const browser = await puppeteer.launch({
             headless: true
         });
 
-    // Create a new page
-    const webPage = await browser.newPage();
+        // Create a new page
+        const webPage = await browser.newPage();
 
-    // Configure the navigation timeout
-    await webPage.setDefaultNavigationTimeout(0);
+        // Configure the navigation timeout
+        await webPage.setDefaultNavigationTimeout(0);
 
-    // Navigate to some website e.g Our Code World
-    await webPage.goto(url, {
-        waitUntil: "networkidle0"
-    });
+        // Navigate to some website e.g Our Code World
+        await webPage.goto(url, {
+            waitUntil: "networkidle0"
+        });
 
-    // Set filename
-    let pageTitle = await webPage.title();
-    pageTitle = pageTitle.replace(/[^a-zA-Z0-9]/g, '_');
-    pageTitle = pageTitle.toLowerCase();
-    const filename = pageTitle + '_' + time + '.pdf';
+        // Set filename
+        let pageTitle = await webPage.title();
+        pageTitle = pageTitle.replace(/[^a-zA-Z0-9]/g, '_');
+        pageTitle = pageTitle.toLowerCase();
+        const filename = pageTitle + '_' + time + '.pdf';
 
-    // Set pdf file name
-    const pdfConfig = {
-        path: filename, // Saves pdf to disk. 
-        format: 'A4',
-        printBackground: true,
-        margin: { // Word's default A4 margins
-            top: '1cm',
-            bottom: '1cm',
-            left: '1cm',
-            right: '1cm'
-        },
-    };
+        // Set pdf file name
+        const pdfConfig = {
+            path: filename, // Saves pdf to disk. 
+            format: 'A4',
+            printBackground: true,
+            margin: { // Word's default A4 margins
+                top: '1cm',
+                bottom: '1cm',
+                left: '1cm',
+                right: '1cm'
+            },
+        };
 
-    // Return the pdf buffer. Useful for saving the file not to disk. 
-    await webPage.pdf(pdfConfig);
+        // Return the pdf buffer. Useful for saving the file not to disk. 
+        await webPage.pdf(pdfConfig);
 
-    await browser.close();
+        await browser.close();
 
-    // Upload pdf to AWS
-    // const urlaws = await s3Client.uploadFile(newname, newname);
+        // Upload pdf to AWS
+        // const urlaws = await s3Client.uploadFile(newname, newname);
 
-    const json = JSON.stringify({
-        target: url,
-        file: filename,
-        // aws: urlaws
-    });
+        json = JSON.stringify({
+            target: url,
+            file: filename,
+            // aws: urlaws
+        });
+
+    } else {
+
+        json = JSON.stringify({
+            target: url,
+            error: 'Key invalid'
+        });
+
+    }
 
     res.contentType("application/json");
     res.send(json);
