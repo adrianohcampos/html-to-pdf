aea1c67d71c3bec9e01b35aec1e5c9cfc0a9479a
Adriano Campos <adrianohcampos@gmail.com>
2021-11-24 17:26:06 -0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require("express");
 const puppeteer = require("puppeteer");
-const s3Client = require('./s3Client');
+//const s3Client = require('./s3Client');
 const fs = require('fs');
 const app = express();
+const time = new Date().getTime();
 
 app.get("/pdf", async (req, res) => {
+
     const url = req.query.target;
-    const time = new Date().getTime();
-    const newname = time + '.pdf';
+
+
 
     // Create an instance of the chrome browser
     const browser = await puppeteer.launch({
@@ -26,6 +28,12 @@ app.get("/pdf", async (req, res) => {
         waitUntil: "networkidle0"
     });
 
+    let pageTitle = await webPage.title();
+    pageTitle = pageTitle.replace(/[^a-zA-Z0-9]/g, '_');
+    pageTitle = pageTitle.toLowerCase();
+
+    const newname = pageTitle + '_' + time + '.pdf';
+
     const pdfConfig = {
         path: newname, // Saves pdf to disk. 
         format: 'A4',
@@ -36,38 +44,27 @@ app.get("/pdf", async (req, res) => {
             left: '1cm',
             right: '1cm'
         },
-        // displayHeaderFooter: true,
-        // headerTemplate: ``,
-        // footerTemplate: `
-        //     <div style="border-top: solid 1px #bbb; width: 100%; font-size: 9px;
-        //         padding: 5px 5px 0; color: #bbb; position: relative;">
-        //         <div style="position: absolute; left: 5px; top: 5px;"><span class="date"></span></div>
-        //         <div style="position: absolute; right: 5px; top: 5px;"><span class="pageNumber"></span>/<span class="totalPages"></span></div>
-        //     </div>
-        // `,
     };
 
-    // await webPage.emulateMedia('screen');
-
-    const pdf = await webPage.pdf(pdfConfig); // Return the pdf buffer. Useful for saving the file not to disk. 
-
-
-
+    // Return the pdf buffer. Useful for saving the file not to disk. 
+    await webPage.pdf(pdfConfig);
 
     await browser.close();
 
+    // Upload pdf to AWS
     // const urlaws = await s3Client.uploadFile(newname, newname);
 
-    const apostila = JSON.stringify({
+    const json = JSON.stringify({
         url: url,
         file: newname,
         // aws: urlaws
     });
 
+    // remove temp file
     //fs.unlinkSync(newname);
 
-    res.contentType("application/pdf");
-    res.send(pdf);
+    res.contentType("application/json");
+    res.send(json);
 })
 
 app.listen(3000, () => {
